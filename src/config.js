c4159f092f50754e7f03f5f1866fb3eec23427a8
AngelIvanOC <20233tn086@utez.edu.mx>
2025-08-27 01:14:08 -0600

    Yaaa

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@ config();
 
 export const PORT = process.env.PORT || 3000;
 export const DB_PORT = process.env.MYSQL_PORT || 3306;
-export const DB_HOST = process.env.MYSQL_HOST || "localhost";
+export const DB_HOST = process.env.MYSQL_HOST || "mysql.railway.internal";
 export const DB_USER = process.env.MYSQL_USER || "root";
-export const DB_PASSWORD = process.env.MYSQL_PASSWORD || "root";
-export const DB_DATABASE = process.env.MYSQL_DATABASE;
+export const DB_PASSWORD =
+  process.env.MYSQL_PASSWORD || "ZlHdKQrsxTOCYaFdKKgWgVsydqeWxsIu";
+export const DB_DATABASE = process.env.MYSQL_DATABASE || "railway";
