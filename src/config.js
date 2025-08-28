f1de4000567ab519296d2316281545204d92e147
AngelIvanOC <20233tn086@utez.edu.mx>
2025-08-27 18:02:08 -0600

    Haber si ya

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const DB_PORT = process.env.MYSQL_PORT || 3306;
-export const DB_HOST = process.env.MYSQL_HOST || "mysql.railway.internal";
-export const DB_USER = process.env.MYSQL_USER || "root";
+export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_HOST = process.env.DB_HOST || "mysql.railway.internal";
+export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD =
-  process.env.MYSQL_PASSWORD || "ZlHdKQrsxTOCYaFdKKgWgVsydqeWxsIu";
-export const DB_DATABASE = process.env.MYSQL_DATABASE || "railway";
+  process.env.DB_PASSWORD || "ZlHdKQrsxTOCYaFdKKgWgVsydqeWxsIu";
+export const DB_DATABASE = process.env.DB_DATABASE || "railway";
--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,6 +1,6 @@
 import { pool } from "../connection.js";
 
 export const ping = async (req, res) => {
-  const [result] = await pool.query("SELECT 1 + 1 AS result");
+  const [result] = await pool.query(`SELECT "Hello Worl" as RESULT`);
   res.json(result[0]);
 };
