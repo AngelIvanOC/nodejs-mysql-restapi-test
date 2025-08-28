7cca2fbd849bf910fae1b24cfd648aa8a591b190
AngelIvanOC <20233tn086@utez.edu.mx>
2025-08-27 20:59:10 -0600

    Resuelto

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ const app = express();
 
 app.use(express.json());
 
-app.use(employeesRoutes);
+app.use("/employees", employeesRoutes);
 
-app.use("/ping", indexRoutes);
+app.use(indexRoutes);
 
 app.use((req, res, next) => {
   res.status(404).json({
